2872906302a9c9fa0336ede2d0f22f7becb136fa
lily <a0976198586@gmail.com>
2021-06-09 23:18:23 +0800

    將檔案推上去

---
+++ js/all.js
@@ -0,0 +1,52 @@
+
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl);
+// });
+
+
+// 表單驗證
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+// (function () {
+//   'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  // var forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+//   Array.prototype.slice.call(forms).forEach(function (form) {
+//       form.addEventListener('submit', function (event) {
+//         if (!form.checkValidity()) {
+//           event.preventDefault();
+//           event.stopPropagation();
+//         }
+
+//         form.classList.add('was-validated');
+//       }, false)
+//     });
+// })();
+
+//提示框和表單驗証
+function init(){
+  let tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  let forms = document.querySelectorAll('.needs-validation')
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    });
+}
+
+init();
