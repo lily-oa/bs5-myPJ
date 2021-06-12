c4656bcc7d2d2f6b88c065edc7357db72b67408b
lily <a0976198586@gmail.com>
2021-06-12 11:35:29 +0800

    update index and js

--- js/all.js
+++ js/all.js
@@ -50,3 +50,67 @@ function init() {
 }
 
 init();
+
+
+var form = document.getElementById('form');
+function heightToTop(ele) {
+  let bridge = ele;
+  let root = document.body;
+  let height = 0;
+  do {
+    height += bridge.offsetTop;
+    bridge = bridge.offsetParent;
+  } while (bridge !== root);
+
+  return height;
+}
+function goToTab() {
+  window.scrollTo({
+    top: heightToTop(document.getElementsByClassName("tab-content")[0]) - 70,
+    behavior: "smooth",
+  });
+}
+
+
+function goForm() {
+  window.scrollTo({
+    top: heightToTop(form) - 170,
+    behavior: "smooth",
+  });
+}
+
+var bottomBtn = document.getElementById("bottomBtn");
+bottomBtn.style.display = "none";
+window.onscroll = function () {
+  const t = document.documentElement.scrollTop || document.body.scrollTop;
+  if (bottomBtn !== null) {
+    if (
+      t >
+        heightToTop(document.getElementsByClassName("tab-content")[0]) - 100 &&
+      t < heightToTop(form)
+    ) {
+      bottomBtn.style.display = "block";
+    } else {
+      bottomBtn.style.display = "none";
+    }
+  }
+};
+
+(function () {
+  "use strict";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
